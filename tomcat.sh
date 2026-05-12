1242a5c7f5be71ce2974d96579e4ac58e9f1653a
Poojith <poojithsingh165@gmail.com>
2026-05-12 23:55:10 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,11 +1,55 @@
-yum install java-17-amazon-corretto -y
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.112/bin/apache-tomcat-9.0.112.tar.gz
-tar -zxvf apache-tomcat-9.0.112.tar.gz
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.112/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.112/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="admin@123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.112/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.112/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-9.0.112/conf/tomcat-users.xml
-sed -i '21d' apache-tomcat-9.0.112/webapps/manager/META-INF/context.xml
-sed -i '22d'  apache-tomcat-9.0.112/webapps/manager/META-INF/context.xml
+#!/bin/bash
+
+# Update packages
+sudo yum update -y
+
+# Install Java 17
+sudo yum install java-17-amazon-corretto -y
+
+# Verify Java
+java -version
+
+# Download Apache Tomcat 9
+wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.112/bin/apache-tomcat-9.0.112.tar.gz
+
+# Extract Tomcat
+tar -xvzf apache-tomcat-9.0.112.tar.gz
+
+# Configure Tomcat manager user
+cat <<EOF >> apache-tomcat-9.0.112/conf/tomcat-users.xml
+
+<role rolename="manager-gui"/>
+<role rolename="manager-script"/>
+<role rolename="admin-gui"/>
+
+<user username="tomcat" password="admin@123"
+roles="manager-gui,manager-script,admin-gui"/>
+
+EOF
+
+# Configure manager app remote access
+cat <<EOF > apache-tomcat-9.0.112/webapps/manager/META-INF/context.xml
+<Context antiResourceLocking="false" privileged="true">
+</Context>
+EOF
+
+# Configure host-manager remote access
+cat <<EOF > apache-tomcat-9.0.112/webapps/host-manager/META-INF/context.xml
+<Context antiResourceLocking="false" privileged="true">
+</Context>
+EOF
+
+# Give execute permissions
+chmod +x apache-tomcat-9.0.112/bin/*.sh
+
+# Start Tomcat
 sh apache-tomcat-9.0.112/bin/startup.sh
+
+# Verify Tomcat
+ps -ef | grep tomcat
+ss -tulnp | grep 8080
+
+echo "Tomcat URL: http://<server-ip>:8080"
+echo "Manager URL: http://<server-ip>:8080/manager/html"
+echo "Username: tomcat"
+echo "Password: admin@123"
