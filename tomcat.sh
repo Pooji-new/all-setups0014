b1e2b9655f0e98a366e3b7913bc8ba8096244761
Poojith <poojithsingh165@gmail.com>
2026-05-13 22:33:38 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -50,7 +50,7 @@ ps -ef | grep tomcat
 ss -tulnp | grep 8080
 
 vi apache-tomcat-9.0.112/webapps/manager/META-INF/context.xml
-cat <<EOF ><Context antiResourceLocking="false" privileged="true"> </Context>
+cat <<EOF > <Context antiResourceLocking="false" privileged="true"> </Context>
 EOF
 
 echo "Tomcat URL: http://<server-ip>:8080"
